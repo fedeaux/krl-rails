c873b4931b4be2d23b2f0018639ab8f1c40d6ec1
Pedro Bernardes <phec06@gmail.com>
2019-11-26 17:02:03 -0200

    better syntax

--- lib/krl.rb
+++ lib/krl.rb
@@ -1,17 +1,22 @@
 module KRL
   class Template
     def initialize(input)
-      @input = input
-      @yaml, @slim = @input.source.split('--KRL--')
+      @contents = Nokogiri::HTML.fragment input.source
+      parse
+    end
+
+    def parse
+      @yaml = @contents.children.select { |child| child.name == 'fields' }.join
+      @slim = @contents.children.select { |child| child.name == 'template' }.join
+
+      @parsed_yaml = @yaml.present? && YAML.load(@yaml) || { fields: {} }
+      @parsed_html = Slim::Template.new { @slim }.render(nil, @parsed_yaml['fields'])
     end
 
     def render
-      yaml = YAML.load(@yaml)
-      html = Slim::Template.new { @slim }.render(nil, yaml['fields'])
-
       [
         '@output_buffer = output_buffer || ActiveSupport::SafeBuffer.new',
-        "@output_buffer.safe_concat('#{html}'.freeze)",
+        "@output_buffer.safe_concat('#{@parsed_html}'.freeze)",
         '@output_buffer'
       ].join("\n")
     end
