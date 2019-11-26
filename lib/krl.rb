883d7f970121c78240ce8e0faf4747f03978f702
Pedro Bernardes <phec06@gmail.com>
2019-11-26 18:25:44 -0200

    update

--- lib/krl.rb
+++ lib/krl.rb
@@ -1,22 +1,24 @@
 module KRL
   class Template
+    attr_reader :fields
+
     def initialize(input)
       @contents = Nokogiri::HTML.fragment input.source
       parse
     end
 
     def parse
-      @yaml = @contents.children.select { |child| child.name == 'fields' }.join
-      @slim = @contents.children.select { |child| child.name == 'template' }.join
+      @raw_yaml = @contents.children.select { |child| child.name == 'fields' }.join
+      @raw_slim = @contents.children.select { |child| child.name == 'template' }.join
 
-      @parsed_yaml = @yaml.present? && YAML.load(@yaml) || { fields: {} }
-      @parsed_html = Slim::Template.new { @slim }.render(nil, @parsed_yaml['fields'])
+      @fields = OpenStruct.new(@raw_yaml.present? && YAML.load(@raw_yaml)['fields'] || {})
+      @html = Slim::Template.new { @raw_slim }.render(nil, @fields.to_h)
     end
 
     def render
       [
         '@output_buffer = output_buffer || ActiveSupport::SafeBuffer.new',
-        "@output_buffer.safe_concat('#{@parsed_html}'.freeze)",
+        "@output_buffer.safe_concat('#{@html}'.freeze)",
         '@output_buffer'
       ].join("\n")
     end
